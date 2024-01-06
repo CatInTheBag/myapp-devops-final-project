a38a736e38edf9abd8f80e302e4eca1dfcedc4e6
Vasil Bachvarov <vassil.bachvarov@gmail.com>
2024-01-06 21:44:01 +0200

    devops-project env for disabling errors added

--- src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
+++ src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
@@ -1,19 +1,10 @@
-/**
- * my app service for retrieving data
- */
 package com.example.myappdevopsfinalproject.service;
 
 import org.springframework.stereotype.Service;
 
-/**
- * My app service.
- */
 @Service
 public final class MyAppService {
 
-  /**
-   * getData() method to return simple message
-   */
   public String getData() {
     return "Hello from my app devops project final.";
   }

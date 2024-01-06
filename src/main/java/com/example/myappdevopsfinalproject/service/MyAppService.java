9c4a4b40304ddd7bd508214bd194531dfb7442af
Vasil Bachvarov <vassil.bachvarov@gmail.com>
2024-01-06 21:38:02 +0200

    devops-project fix for java code linting

--- src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
+++ src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
@@ -1,13 +1,19 @@
+/**
+ * my app service for retrieving data
+ */
 package com.example.myappdevopsfinalproject.service;
 
 import org.springframework.stereotype.Service;
 
 /**
- * My app service
+ * My app service.
  */
 @Service
 public final class MyAppService {
 
+  /**
+   * getData() method to return simple message
+   */
   public String getData() {
     return "Hello from my app devops project final.";
   }

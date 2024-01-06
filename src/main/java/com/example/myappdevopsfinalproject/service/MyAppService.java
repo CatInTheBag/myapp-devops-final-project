07cec7ef06aff41bd0cac78b40a2d187e501b79c
Vasil Bachvarov <vassil.bachvarov@gmail.com>
2024-01-06 20:35:17 +0200

    devops project test for unused variable

--- src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
+++ src/main/java/com/example/myappdevopsfinalproject/service/MyAppService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyAppService {
     public String getData(){
+        var someVariable = "test";
         return "Hello from my app devops project final.";
     }
 }
